b5938cb565cdf91872a7acc64c6fc3809c4b5158
Daniel Xoconostle <dxoconos@gmail.com>
2020-08-12 17:24:09 -0500

    [module-1 lab-numpy]Daniel Xoconostle

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,73 +1,88 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
-
+print(np.__version__)
+print(np.show_config())
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
+a = np.random.randint(1,10,[2,3,5])
 
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.random.randint(1,10,[5,2,3])
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
+if a.shape == b.shape:
+    print(True)
+else:
+    print(False)
 
 
 
 #8. Are you able to add a and b? Why or why not?
-
+"""
+print(a+b)
+It is not possible to add a +b due to their having different shapes.
+"""
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
+c = b.reshape(2,3,5)
+print("\nb:\n",b)
+print("\nc:\n",c)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+d = a+c
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
+print("\na:\n",a)
+print("\nd:\n",d)
 
 
 
 #12. Multiply a and c. Assign the result to e.
-
-
+e = a*c
+print("e\n",e)
 
 #13. Does e equal to a? Why or why not?
-
-
+if a.shape == e.shape:
+    print(True)
+else:
+    print(False)
+"""In shape they are equal, that would be due to matrix multiplication stuff"""
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
-
+d_max = d.max()
+d_min = d.min()
+d_mean=d.mean()
+print(f'dmax {d_max}\tdmin {d_min}\tdmean {d_mean}')
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
+f = np.empty([2,3,5])
 
 
 
-
+.
 """
-#16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
-If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
+#16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in fIf a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
 If a value equals to d_mean, assign 50 to the corresponding value in f.
 Assign 0 to the corresponding value(s) in f for d_min in d.
 Assign 100 to the corresponding value(s) in f for d_max in d.
@@ -76,9 +91,8 @@ Note: you don't have to use Numpy in this question.
 """
 
 
-
-
 """
+
 #17. Print d and f. Do you have your expected f?
 For instance, if your d is:
 array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
